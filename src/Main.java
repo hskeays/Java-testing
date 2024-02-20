d5982c7c536dff1ecae69478aee96cebe602fa8d
hskeays <hskeays@gmail.com>
2024-02-20 18:19:31 -0500

    binary search

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 2, 3, 4, 5};
+        int value = 3;
+        int answer = binarySearch(array, value);
+
+        if (binarySearch(array, value) == -1) {
+            System.out.println("Value does not exist in the array");
+        } else {
+            System.out.println("The index with value " + value + ": " + answer);
+        }
+    }
+
+    static int binarySearch(int[] array, int searchValue) {
+        Arrays.sort(array);
+        int min = 0;
+        int max = array.length - 1;
+
+        while (min <= max) {
+            int mid = min + (max - min) / 2;
+
+            if (array[mid] == searchValue) {
+                return mid;
+            } else if (array[mid] < searchValue) {
+                min = mid + 1;
+            } else {
+                max = mid - 1;
+            }
+        }
+        return -1;
     }
 }
